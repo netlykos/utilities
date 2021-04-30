7856709215b9d2b3dba70bd8ec5804ff938e2f6d
Adi B <netlykos@sdf.org>
2021-04-30 23:27:56 +0100

    correction from fetchSize to rowSize, added develop to build actions

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# utilities
+# Utilities
 
-Utility project to do silly stuff with spring
+![utilities workflow](https://github.com/netlykos/utilities/actions/workflows/gradle.yml/badge.svg)
+
+Utility project to experiment with and test out platforms/infrastructure and test out spring/gradle and/or other technology features.
+
+
--- dbquery/src/main/java/org/netlykos/dbquery/dao/JDBCQuery.java
+++ dbquery/src/main/java/org/netlykos/dbquery/dao/JDBCQuery.java
@@ -8,27 +8,35 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+
+/**
+ * @author Adi B (@netlykos)
+ *
+ * This class is used to run user provided queries against the given data source.
+ */
 @Repository
 public class JDBCQuery {
 
   @Autowired
   private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-  
-  @Value("${org.netlykos.dbquery.jdbcQuery.maxFetchSize:100}")
-  private int maxFetchSize;
+
+  @Value("${org.netlykos.dbquery.jdbcQuery.maxResultSize:50}")
+  private int maxResultSize;
 
   public Map<String, Object> queryForMap(String query, Map<String, Object> parameters) {
+    namedParameterJdbcTemplate.getJdbcTemplate().setFetchSize(-1);
     return namedParameterJdbcTemplate.queryForMap(query, parameters);
   }
 
   public List<Map<String, Object>> queryForList(String query, Map<String, Object> parameters) {
-    return queryForList(query, parameters, maxFetchSize);
+    return queryForList(query, parameters, maxResultSize);
   }
 
-  public List<Map<String, Object>> queryForList(String query, Map<String, Object> parameters, int fetchSize) {
-    // fetch size cannot be greater than maxFetchSize
-    int queryFetchSize = fetchSize < maxFetchSize ? fetchSize : maxFetchSize;
-    namedParameterJdbcTemplate.getJdbcTemplate().setFetchSize(queryFetchSize);
+  public List<Map<String, Object>> queryForList(String query, Map<String, Object> parameters, int resultSize) {
+    // fetch size cannot be infinite and cannot be larger than the maxResultSize defined
+    int maxRows = resultSize < 1 ?  maxResultSize : resultSize > maxResultSize ? maxResultSize : resultSize;
+    namedParameterJdbcTemplate.getJdbcTemplate().setFetchSize(maxRows);
+    namedParameterJdbcTemplate.getJdbcTemplate().setMaxRows(maxRows);
     return namedParameterJdbcTemplate.queryForList(query, parameters);
   }
 
